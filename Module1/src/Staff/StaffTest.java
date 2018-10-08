1bb5e08fa58979b6a163740f3d19012de6f61f4e
= <=>
2018-10-08 22:35:08 +0700

    Person và các lớp con

---
+++ Module1/src/Staff/StaffTest.java
@@ -0,0 +1,10 @@
+package Staff;
+
+public class StaffTest {
+    public static void main(String[] args) {
+        Staff staff = new Staff();
+        System.out.println(staff);
+        staff = new Staff("Tran Phu",25000,"Hà","Bac Giang");
+        System.out.println(staff);
+    }
+}
